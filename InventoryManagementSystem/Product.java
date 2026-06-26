7df70c4d8a94dd77eba246c9bc744cb49e3c42cf
Atika <www.aatikayasin@gmail.com>
2026-06-26 23:53:56 +0500

    Initializing the constructor and encapsulating

--- InventoryManagementSystem/Product.java
+++ InventoryManagementSystem/Product.java
@@ -2,5 +2,23 @@ import java.util.*;
 public class Product {
     public static void main(String[] args){
         System.out.println("This is the product list.");
+        private int id;
+        private String name;
+        private double price;
+        private int quantity;
+        private int threshold;
+        private String supplierName;
+
+
+        //constructor
+        public Product(int id, String name, double price, int quantity, int threshold, String supplierName){
+                this.id = id;
+                this.name = name;
+                this.price = price;
+                this.quantity = quantity;
+                this.threshold = threshold;
+                this.supplierName = supplierName;
+
+               }
     }
 }
